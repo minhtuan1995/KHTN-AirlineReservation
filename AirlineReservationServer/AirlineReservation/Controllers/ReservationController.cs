57ca0e1fcb611cde9b03412a25e76c6f320fd699
vnbao <killerbee080795@gmail.com>
2016-10-25 16:21:30 +0700

    Cập nhật API Airline Reservation Controller.cs
    GET api/reservation
    POST api/reservation

--- AirlineReservationServer/AirlineReservation/Controllers/ReservationController.cs
+++ AirlineReservationServer/AirlineReservation/Controllers/ReservationController.cs
@@ -13,6 +13,53 @@ namespace AirlineReservation.Controllers
 {
     public class ReservationController : ApiController
     {
-        
+        //entity object
+        AirlineDatabaseEntities entity = new AirlineDatabaseEntities();
+
+        // GET api/reservation/5
+        [HttpGet]
+        public List<SeatEntity> Get(int id)
+        {
+            List<SeatEntity> seats = entity.Seats.Where(x => x.ScheduleID == id).Select(x => new SeatEntity
+            {
+                ScheduleID = (int)x.ScheduleID,
+                SeatID = x.SeatID,
+                SeatType = x.SeatType,
+                CurrentStatus = (int)x.CurrentStatus
+            }).ToList();
+
+            return seats;
+        }
+
+        // POST api/reservation
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+            SeatEntity seat = JsonConvert.DeserializeObject<SeatEntity>(value);
+
+            Seat updateSeat = entity.Seats.Where(x => x.SeatID == seat.SeatID).First();
+            //only reserve if seat is still available
+            if (updateSeat.CurrentStatus == 1 || updateSeat.CurrentStatus == 0)
+            {
+                //add user record
+                AirlineReservation.Models.User user = new User();
+                user.Name = seat.Name;
+                user.Age = seat.Age;
+                user.Email = seat.Email;
+                entity.Users.Add(user);
+                entity.SaveChanges();
+
+                int userID = entity.Users.OrderByDescending(x => x.UserID).Select(x => x.UserID).First();
+
+                //Modify seats record
+                entity.Seats.Attach(updateSeat);
+                updateSeat.UserID = userID;
+                updateSeat.CurrentStatus = 2;
+                entity.Entry(updateSeat).State = EntityState.Modified;
+                entity.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            return Request.CreateResponse(HttpStatusCode.InternalServerError);
+        }
     }
 }
