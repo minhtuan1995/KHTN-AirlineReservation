0ee92d0ca05cb80b33efd389942d76da4454d3cf
minhtuan1995 <daominhtuan1995@gmail.com>
2016-10-25 16:48:27 +0700

    Cập nhật chức năng Lịch bay
    - Thêm lịch trình bay
    - Hiển thị danh sách lịch trình bay

--- AirlineReservationClient/js/Airlines.js
+++ AirlineReservationClient/js/Airlines.js
@@ -12,12 +12,20 @@ app.controller('airlineController', function ($scope, $http) {
     //Default Values
     $scope.divHome = true;
     $scope.divAirlines = false;
-
+    $scope.divSchedule = false;
+    $scope.divReservation = false;
+    $scope.divSeats = false;
     $scope.inp_airCode = '';
     $scope.inp_airPE = '';
     $scope.inp_airPF = '';
     $scope.inp_airSE = '';
     $scope.inp_airSF = '';
+    //schedule
+    $scope.AirplaneID = '';
+    $scope.inp_airDep = '';
+    $scope.inp_airArr = '';
+    $scope.inp_airDepDate = '';
+    $scope.inp_airArrDate = '';
 
     //show home
     $scope.showHome = function () {
@@ -40,13 +48,84 @@ app.controller('airlineController', function ($scope, $http) {
                 );
     };
 
+    //function to show schedule input and data
+    $scope.showSchedule = function () {
+        $scope.hideAll();
+
+        $http.get(url + "api/Schedule/5")
+        .success(function (response) {
+            $scope.airCodes = response;
+        }).error(
+                    function (data, status, headers, config) {
+                        toastr.error = "Error retrieving Airline data!";
+                    }
+                );
+
+        $http.get(url + "api/Schedule")
+        .success(function (response) {
+            $scope.dataSchedule = response;
+            //console.log(response);
+            $scope.divSchedule = true;
+        }).error(
+                    function (data, status, headers, config) {
+                        toastr.error = "Error retrieving data!";
+                    }
+                );
+    };
+
+
+
     //hide all divs
     $scope.hideAll = function () {
         $scope.divHome = false;
         $scope.divAirlines = false;
+        $scope.divSchedule = false;
     };
 
-        //function to add airline
+    //function to add schedule
+    $scope.AddSchedule = function () {
+        if (!$scope.AirplaneID.length
+            || !$scope.inp_airDep.length
+            || !$scope.inp_airArr.length
+            || !$scope.inp_airDepDate.length
+            || !$scope.inp_airArrDate.length
+            ) {
+            toastr.error = "Some fields are blank or have invalid values!";
+        }
+        else {
+            postString = '{"AirplaneID":' + $scope.AirplaneID + ',';
+            postString += '"Airport_Departure":"' + $scope.inp_airDep + '",';
+            postString += '"Airport_Arrival":"' + $scope.inp_airArr + '",';
+            postString += '"Time_Departure":"' + $scope.inp_airDepDate + '",';
+            postString += '"Time_Arrival":"' + $scope.inp_airArrDate + '"}';
+            //$scope.error = postString;
+            $http({
+                method: 'POST',
+                url: url + 'api/Schedule',
+                data: "=" + postString,
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+            }).success(
+                    function (data, status, headers, config) {
+                        $scope.AirplaneID = '';
+                        $scope.inp_airDep = '';
+                        $scope.inp_airArr = '';
+                        $scope.inp_airDepDate = '';
+                        $scope.inp_airArrDate = '';
+                        $scope.error = '';
+                        $scope.hideAll();
+                        $scope.showSchedule();
+                        toastr.success('Schedule added successfully!!')
+                    }
+                ).error(
+                    function (data, status, headers, config) {
+                        toastr.error = "Error saving schedule!";
+                    }
+                );
+        }
+    };
+
+
+    //function to add airline
     $scope.AddAirline = function () {
         //$.ajax({
         //    type: "POST",
