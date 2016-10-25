2be6a22fa62db3aa48997ee9e2d2d028678a3e2d
vnbao <killerbee080795@gmail.com>
2016-10-25 16:00:44 +0700

    Airline Reservation Server controller
    code file AirPlane controller.cs

--- AirlineReservationServer/AirlineReservation/Controllers/AirPlaneController.cs
+++ AirlineReservationServer/AirlineReservation/Controllers/AirPlaneController.cs
@@ -15,6 +15,43 @@ namespace AirlineReservation.Controllers
     public class AirPlaneController : ApiController
     {
         
-        
+        //entity object
+        AirlineDatabaseEntities entity = new AirlineDatabaseEntities();
+
+        // GET api/airplane
+        [HttpGet]
+        public List<AirplaneEntity> Get()
+        {
+            List<AirplaneEntity> airplanes;
+            airplanes = entity.AirPlanes.Select(x => new AirplaneEntity
+            {
+                AirplaneID = x.AirplaneID,
+                AirplaneCode = x.AirplaneCode,
+                Price_Economy = x.Price_Economy,
+                Price_FirstClass = x.Price_FirstClass,
+                Seats_Economy = x.Seats_Economy,
+                Seats_FirstClass = x.Seats_FirstClass
+            }).ToList();
+
+            return airplanes;
+        }
+
+        // POST api/airplane
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody]string value)
+        {
+
+            try
+            {
+                var obj = JsonConvert.DeserializeObject<AirPlane>(value);
+                entity.AirPlanes.Add(obj);
+                entity.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
     }
 }
